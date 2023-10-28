c7f7133788d692295583c281244a438921ffb9a3
Matthew Robillard <robillard.matthew22@gmail.com>
2023-10-28 10:11:27 -0700

    Setup -M

---
+++ Frontend/Frontend.py
@@ -0,0 +1,5 @@
+#User draws in a color, this is what it means 
+#Red Primitive variables like "+" in python or "3", if we can say exec(primitive)
+#Blue User Defined, 
+#Green IDK maybe like something for loops or function declaration
+#
